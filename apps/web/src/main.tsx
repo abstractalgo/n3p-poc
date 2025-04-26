c7b083f600b489e56c5a92b589fd32b0b6da5bfa
Dragan Okanovic <dragan.okan@gmail.com>
2025-04-26 12:55:55 +0200

    wip

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,14 +1,17 @@
 import { createRoot } from "react-dom/client";
 import "./style.css";
+import "@repo/editor/style.css";
 import typescriptLogo from "/typescript.svg";
 import { Header, Counter } from "@repo/ui";
+import { Editor } from "@repo/editor";
 
 const App = () => (
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <Editor />
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src="/vite.svg" className="logo" alt="Vite logo" />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
+    <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
       <img
         src={typescriptLogo}
         className="logo vanilla"
--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -2,7 +2,6 @@ import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -23,7 +22,7 @@ export const config = [
   },
   {
     plugins: {
-      onlyWarn,
+      // onlyWarn,
     },
   },
   {
